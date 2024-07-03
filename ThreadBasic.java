0b26240e90f349fcf8f3329c7e36f2f5e0e65649
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-03 16:13:13 +0530

    Runnable Interface

--- ThreadBasic.java
+++ ThreadBasic.java
@@ -24,6 +24,24 @@ class AlternateThreadNumberPrinter extends Thread{
         System.out.println(this.number + " " + Thread.currentThread().getName());
     }
 }
+class SingleNumberPrinter extends Thread{
+    static int num = 0;
+    @Override
+    public void run(){
+        num++;
+        System.out.println(num + " " + Thread.currentThread().getName());
+    }
+}
+class Student implements Runnable{
+    int num;
+    public Student(int num){
+        this.num = num;
+    }
+    @Override
+    public void run() {
+        System.out.println(this.num + " " + Thread.currentThread().getName());
+    }
+}
 public class ThreadBasic{
     public static void main(String[] args) {
         // System.out.println(Thread.currentThread().getName());
@@ -37,10 +55,17 @@ public class ThreadBasic{
         // numberPrinter1.start();
         // numberPrinter2.start();
 
-        for (int j=1; j<=100000; j++){
-            AlternateThreadNumberPrinter alternateThreadNumberPrinter = new AlternateThreadNumberPrinter(j);
-            alternateThreadNumberPrinter.start();
-        }
+        // for (int j=1; j<=100000; j++){
+        //     AlternateThreadNumberPrinter alternateThreadNumberPrinter = new AlternateThreadNumberPrinter(j);
+        //     alternateThreadNumberPrinter.start();
+        // }
 
+        // for (int j=0; j<100; j++){
+        //     SingleNumberPrinter singleNumberPrinter = new SingleNumberPrinter();
+        //     singleNumberPrinter.start();
+        // }
+
+        Thread th = new Thread(new Student(0));
+        th.start();
     }
 }
